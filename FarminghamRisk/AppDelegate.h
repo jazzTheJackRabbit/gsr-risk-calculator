d53fa40c4284a585ef11685ff85348dfbb2aeef6
The Jazz <amogh.91@gmail.com>
2015-01-17 14:05:57 -0800

    Initial Commit

---
+++ FarminghamRisk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FarminghamRisk
+//
+//  Created by Amogh Param on 1/17/15.
+//  Copyright (c) 2015 Amogh Param. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FarminghamRisk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FarminghamRisk
+//
+//  Created by Amogh Param on 1/17/15.
+//  Copyright (c) 2015 Amogh Param. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
